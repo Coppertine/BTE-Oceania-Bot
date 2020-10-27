cd0293311b439c76baf0120688edeec3fa843f4d
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-10-27 16:29:09 +1000

    Add embed for greeting and member counts

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,30 @@
+module.exports = async (client, member) => {
+    const guild = member.guild;
+    const memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+    const welcomeChannel = client.channels.cache.get('740865155243704412');
+    
+    welcomeChannel.send(member.tag);
+    welcomeChannel.send({ embed: {
+        title: 'Welcome!',
+        color: client.info.embedHexcode,
+        description: member.tag,
+        thumbnail: member.avatar,
+        fields: [
+            {
+                title:'\u200b',
+                description: 'Please read <#740865161749069874> and <#740865157189861437> to get started!'
+            },
+            {
+                title:'\u200b',
+                description: 'Don\'t forget to verify yourself using the :door: at <#740865157189861437>!'
+            },
+            {
+                title:'\u200b',
+                description: 'You are the ${memberCount}th member!'
+            }
+        ]}});
+    
+    
+    client.channels.cache.get('746584674180268073').setName(`👫 All Members: ${memberCount}`);
+    
+}
